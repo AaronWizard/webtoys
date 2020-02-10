6403e6d192cf6969848838899a7ade6ef7c97e26
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-10 18:11:24 -0400

    SCSS support

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
 				test: /\.html$/,
 				loader: 'html-loader',
 			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
 		],
 	},
 	resolve: {
