3abcbb700b18490fb59af5e398daa27b916d34b8
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-10 13:59:42 -0400

    Added page not found. Surrounded routes with Switch.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import About from './about/About';
+import PageNotFound from './pagenotfound/PageNotFound';
+
 import ViewDogs from './viewdogs/ViewDogs';
 
 const Main = () => (
 	<Router basename="/">
 		<main>
-			<Route exact path="/" component={About} />
-			<Route path="/viewdogs" component={ViewDogs} />
+			<Switch>
+				<Route exact path="/" component={About} />
+				<Route exact path="/viewdogs" component={ViewDogs} />
+				<Route component={PageNotFound} />
+			</Switch>
 		</main>
 	</Router>
 );
---
+++ src/components/pagenotfound/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HomeLink from '../HomeLink';
+
+const PageNotFound = () => (
+	<div>
+		<p>Page not found</p>
+		<HomeLink />
+	</div>
+);
+
+export default PageNotFound;
