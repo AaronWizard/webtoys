5e044603fee1ee4de43a4abb9373847595b5f6e4
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-05-20 05:35:11 +0000

    Updated ESLint

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
+import babelParser from "@babel/eslint-parser";
+
+export default [
+	js.configs.recommended,
+
+	{
+		files: ["**/*.js", "**/*.jsx"],
+
+		languageOptions: {
+			parser: babelParser,
+
+			parserOptions: {
+				requireConfigFile: false,
+
+				babelOptions: {
+					presets: [
+						"@babel/preset-env",
+						"@babel/preset-react"
+					]
+				},
+
+				ecmaVersion: 2018,
+				sourceType: "module",
+				ecmaFeatures: {
+					jsx: true
+				}
+			},
+
+			globals: {
+				...globals.browser,
+				Atomics: "readonly",
+				SharedArrayBuffer: "readonly"
+			}
+		},
+
+		plugins: {
+			react: reactPlugin
+		},
+
+		settings: {
+			react: {
+				version: "detect"
+			}
+		},
+
+		rules: {
+			"no-tabs": ["error", { allowIndentationTabs: true }],
+			"indent": ["error", "tab"],
+			"max-len": ["error", { code: 80 }],
+			"brace-style": ["error", "allman"],
+
+			"react/jsx-indent": [2, "tab"],
+			"react/jsx-indent-props": [2, "tab"],
+			"react/static-property-placement": [
+				"error",
+				"static public field"
+			]
+		}
+	}
+];
