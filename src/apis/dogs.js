7d91a29e040b641f79a6c8ed933e897be3f03fb7
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-05-20 20:30:37 +0000

    Error handling in getDogs

--- src/apis/dogs.js
+++ src/apis/dogs.js
@@ -2,21 +2,35 @@ import axios from 'axios';
 
 const url = 'https://dog.ceo/api/breeds/image/random/';
 
+const maxAllowedErrors = 5;
+
 const getDogs = async (currentDogs, maxDogs) =>
 {
 	let result = currentDogs;
+	let errorCount = 0;
 
-	while (result.length < maxDogs) 
+	while (result.length < maxDogs)
 	{
-		const resp = await axios.get(`${url}${maxDogs - result.length}`);
-
-		const newDogs = resp.data.message;
-		const allDogs = result.concat(newDogs);
-		const filteredDogs = allDogs.filter((d, i) => (
-			(allDogs.indexOf(d) === i) && (d.split('.').pop() !== 'txt')
-		));
-
-		result = filteredDogs;
+		try
+		{
+			const resp = await axios.get(`${url}${maxDogs - result.length}`);
+			const newDogs = resp.data.message;
+			const allDogs = result.concat(newDogs);
+			const filteredDogs = allDogs.filter((d, i) => (
+				(allDogs.indexOf(d) === i) && (d.split('.').pop() !== 'txt')
+			));
+			result = filteredDogs;
+			errorCount = 0;
+		}
+		catch (error)
+		{
+			console.error(`Error loading dog pics: ${error}`);
+			++errorCount;
+			if (errorCount > maxAllowedErrors)
+			{
+				break;
+			}
+		}
 	}
 
 	return result;
